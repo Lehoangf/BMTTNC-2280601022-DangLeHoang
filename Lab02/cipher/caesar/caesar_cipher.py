532314ead3bc303e7cd36d7eb3783b5d96f4377b
Lehoangf <danglehoang.k22@gmail.com>
2025-03-13 13:10:11 +0700

    lab02-vigenere

--- Lab02/cipher/caesar/caesar_cipher.py
+++ Lab02/cipher/caesar/caesar_cipher.py
@@ -1,33 +1,28 @@
-from string import ascii_uppercase
-
-ALPHABET = list(ascii_uppercase)
+from cipher.caesar import ALPHABET
 
 class CaesarCipher:
     def __init__(self):
         self.alphabet = ALPHABET
-
+        
     def encrypt_text(self, text: str, key: int) -> str:
-        text = text.upper()
         alphabet_len = len(self.alphabet)
+        text = text.upper()
         encrypted_text = []
         for letter in text:
-            if letter in self.alphabet:
-                letter_index = self.alphabet.index(letter)
-                letter_cipher = (letter_index + key) % alphabet_len
-                encrypted_text.append(self.alphabet[letter_cipher])
-            else:
-                encrypted_text.append(letter)  # Giữ nguyên ký tự không có trong bảng chữ cái
-        return "".join(encrypted_text)
-
+            letter_index = self.alphabet.index(letter)
+            output_index = (letter_index + key) % alphabet_len
+            outout_letter = self.alphabet[output_index]
+            encrypted_text.append(outout_letter)
+        return ''.join(encrypted_text)
+    
     def decrypt_text(self, text: str, key: int) -> str:
-        text = text.upper()
         alphabet_len = len(self.alphabet)
+        text = text.upper()
         decrypted_text = []
         for letter in text:
-            if letter in self.alphabet:
-                letter_index = self.alphabet.index(letter)
-                letter_cipher = (letter_index - key) % alphabet_len
-                decrypted_text.append(self.alphabet[letter_cipher])
-            else:
-                decrypted_text.append(letter)  # Giữ nguyên ký tự không có trong bảng chữ cái
+            letter_index = self.alphabet.index(letter)
+            output_index = (letter_index - key) % alphabet_len
+            outout_letter = self.alphabet[output_index]
+            decrypted_text.append(outout_letter)
         return ''.join(decrypted_text)
+    
